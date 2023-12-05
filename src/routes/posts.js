14c18c4fd0f70714691b6cb86c2fa7c7b6a2ca3e
alejandro_junio29 <alejandro_junio29@hotmail.com>
2023-12-05 01:45:47 -0600

    Added the controllers of posts and users

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router() //es lo que nos permite crear rutas fuera de nuestro app.js que podemos generar como modulos
 
 router.get('/', (req, res)=>{
-    res.status(200).send("Hola mundo desde users")
+    res.status(200).send("Hola mundo desde get posts")
 })
 
 module.exports = router
