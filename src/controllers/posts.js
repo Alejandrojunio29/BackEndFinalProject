138f59556105404b32e5a0a1ba9cf2f8556f7c64
alejandro_junio29 <alejandro_junio29@hotmail.com>
2023-12-05 03:53:18 -0600

    added the post-endpoint working

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,3 +1,5 @@
+const Post = require('../models/posts')
+
 module.exports = {
     getAll: async (req, res, next)=>{
         next({status: 200, send: {msg:"Publicacio", data: []}})
@@ -6,7 +8,12 @@ module.exports = {
         next({status: 200, send: {msg:"Publicacion encontrada", data: {}}})
     },
     post: async (req, res, next)=>{
-        next({status: 200, send: {msg:"Publicacion creada", data: {}}})
+        try{
+            let post = await Post.create(req.body)
+            next({status: 201, send: {msg:"Publicacion creada", data: post}})
+        }catch (error){
+            next({status: 400, send: {msg:"Publicacion no creada", data:error }})
+        }
     },
     put: async (req, res, next)=>{
         next({status: 200, send: {msg:"Publicacion actualizada", data: {}}})
