ad31c68f77595abb85a39cdf84191490568190f8
alejandro_junio29 <alejandro_junio29@hotmail.com>
2023-12-05 02:15:37 -0600

    Db connected

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config()
 const express = require ('express')
 const morgan = require('morgan')
 const routes = require('./src/routes/index')
+const db = require('./src/utils/db')
 const app = express()
 const port = process.env.PORT || 3002
+
+db.connect()
 
 app.use(express.json()) //Vamos a recibir bodies tipo JSon, nuestro backend Api rest
 app.use(morgan('dev'))
